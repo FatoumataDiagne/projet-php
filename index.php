76f4749701f2d7a1d77cc22f2f63ba3c3b45d5d0
FatoumataDiagne <diagnefatoumata497@gmail.com>
2024-12-17 23:39:10 +0100

    secondcommit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
  // Fonction principale pour afficher le message 
 function main() { 
- echo "Hello, GitHub!"; 
+ echo "Hello welcome in mygit add index.php GitHub!"; 
 } 
 // Appel de la fonction principale 
 main(); 
